a9059ae536ba23a9d93a46b48c7c1f1d2160c920
Chrysaint <binclear0904@gmail.com>
2024-04-02 01:38:04 +0300

    first commit

---
+++ src/components/header.php
@@ -0,0 +1,36 @@
+<?php
+include_once('./services/dbconnect.php');
+?>
+
+<header class="header">
+    <nav class="header__nav">
+        <ul class="header__nav__list">
+            <li class="header__nav__item">
+                <a href="" class="header__nav__link">
+                    Мужчинам
+                </a>
+            </li>
+            <li class="header__nav__item">
+                <a href="" class="header__nav__link">
+                    Женщинам
+                </a>
+            </li>
+            <li class="header__nav__item">
+                <a href="" class="header__nav__link">
+                    Каталог
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <a class="header__logo" href="/">Lina</a>
+    <div class="header__right">
+        <ul class="header__nav__list">
+            <li class="header__nav__item header__nav__item_user">
+                user
+            </li>
+            <li class="header__nav__item header__nav__item_cart">
+                cart
+            </li>
+        </ul>
+    </div>
+</header>
